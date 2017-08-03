1c540fed00f934e300df16156272e3946d7f24fe
Nathan <xianan1013@gmail.com>
2017-08-02 22:16:15 -0400

    update

---
+++ Test/src/test.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<Integer> list = new ArrayList<>();
+		
+		Map<Character, Integer> map = new HashMap<>();
+	
+
+	}
+
+}
