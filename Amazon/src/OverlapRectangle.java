091203b496167a07368ea3e1b63cee5630378e34
Nathan <xianan1013@gmail.com>
2017-09-05 23:10:52 -0400

    update

--- Amazon/src/OverlapRectangle.java
+++ Amazon/src/OverlapRectangle.java
@@ -4,6 +4,21 @@ public class OverlapRectangle {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
+	}
+	
+	public static boolean doOverlap(Point l1, Point r1, Point l2, Point r2)
+	{
+	    // If one rectangle is on left side of other
+	    if (l1.x > r2.x || l2.x > r1.x)
+	        return false;
+	 
+	    // If one rectangle is above other
+	    if (l1.y < r2.y || l2.y < r1.y)
+	        return false;
+	 
+	    return true;
 	}
 
 }
