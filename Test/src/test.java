f4dae4d20406f860a603ca4a858ec46db0751e7a
Nathan <xianan1013@gmail.com>
2017-12-03 15:31:48 -0500

    updare

--- Test/src/test.java
+++ Test/src/test.java
@@ -4,7 +4,15 @@ public class test {
 
 	public static void main(String[] args) {
 		
-		String s = "aaabbbccc";
-		System.out.println(s.indexOf("a"));
+		int[][] matrix = {{1,2}, {3, 4}};
+		
+		int[][] matrix2 = matrix;
+		
+		for (int[] v : matrix2){
+			for (int v2 : v) {
+				System.out.println(v2);
+			}
+		}
+			
 	}
 }
