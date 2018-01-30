e401cc08a82910d6a449817c9ac31998bf078dc1
Nathan <xianan1013@gmail.com>
2018-01-30 15:35:18 -0500

    update

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/18/d04ed1d4fa0500181f09f9d53db55fd6
@@ -0,0 +1,9 @@
+
+public class Sorter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Test/src/test.java
+++ Test/src/test.java
@@ -1,23 +1,19 @@
+
 import java.util.*;
 
-public class test {
 
-	public static void main(String[] args) {
-		
-		
-		PriorityQueue<Integer> res = new PriorityQueue<>();
-		int N = 100000;
-		
-		for (int i = 0; Math.pow(2, i) < N; i++) {
-			for (int j = 0; Math.pow(5, j) < N; j++) {
-				int tmp = (int)(Math.pow(2, i) * Math.pow(5, j));
-				if (tmp > N) break;
-				res.add(tmp);
-			}
-		}
-		
-		while (!res.isEmpty()) {
-			System.out.println(res.poll());
-		}
-	}
+
+public class test {
+   
+    public static void main (String [] args) {
+    	Set<String> set = new LinkedHashSet<String>();
+        set.add("3");
+        set.add("1");
+        set.add("3");
+        set.add("2");
+        set.add("3");
+        set.add("1");
+        set.forEach(s -> System.out.print(s+"-"));
+
+    }
 }
