1fa966b5dfec81ebb4ca88084731787f17985342
Nathan <xianan1013@gmail.com>
2018-06-30 10:11:14 -0700

    update

--- Test/src/test.java
+++ Test/src/test.java
@@ -5,11 +5,30 @@ import java.util.*;
 
 
 public class test {
+	
+	static class TrieNode {
+		TrieNode[] children;
+		String word;
+		
+		public TrieNode() {
+			children = new TrieNode[26];
+			word = null;
+		}
+	}
    
     public static void main (String [] args) {
     	
-    	System.out.println((int)3.7);
+    	Map<Integer, TrieNode> map = new HashMap<>();
     	
+    	map.put(1, new TrieNode());
     	
+    	TrieNode node = map.get(2);
+    	
+    	if (node == null) {
+    		map.put(2, new TrieNode());
+    		map.get(2).word = "hahaha";
+    	}
+    	
+    	System.out.println(map.get(2).word);
     }
 }
---
+++ Test/src/test2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public abstract class test2 {
+	ArrayList<Double> list;
+	
+	public test2() {
+		list = new ArrayList<>();
+	}
+}
